5aef4db5b4878fc53512901375baaf6612ef2ab9
Dav Singh <davinder.cheema@ymail.com>
2020-10-03 14:40:11 +0100

    Added images and name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import portrait from './images/IMG_20150715_111754~2.jpg';
+
 function App() {
   return (
-    <div>Coming Soon</div>
+    <div className="flex__container">
+      <div className="grid__container">
+        <main className="content">
+          <div className="circular--landscape">
+            <img src={portrait} alt="Dav Singh (daxz1)" className="portrait"/>
+          </div>
+          <div>
+            <p>Dav Singh (daxz1)</p>
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
 
